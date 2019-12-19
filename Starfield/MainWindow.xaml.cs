85a73b5b607f67e411b0781c88071b5066355029
Keelah <foxlider.atom@gmail.com>
2019-12-19 16:10:58 +0100

    Performance optimization

--- Starfield/MainWindow.xaml.cs
+++ Starfield/MainWindow.xaml.cs
@@ -22,21 +22,14 @@ namespace Starfield
         private const    int             TARGET_FPS = 60;
         private readonly List<Star>      stars      = new List<Star>();
         readonly         DispatcherTimer timer      = new DispatcherTimer();
-        readonly         DispatcherTimer fpsTimer   = new DispatcherTimer();
         readonly         DispatcherTimer fpsTimer2  = new DispatcherTimer();
         private          int             framesExecuted;
-        private          int             frameCount;
         private          double          halfWidth;
         private          double          halfHeight;
         private          double          kValue = 128.0;
-        private double offsetH;
-        private double offsetV;
 
         private readonly Stopwatch clock;
-        private readonly Stopwatch watch;
 
-        readonly DispatcherTimer offsetterDispatcher = new DispatcherTimer();
-        private readonly Stopwatch offsetterWatch;
         public MainWindow()
         {
             InitializeComponent();
@@ -44,68 +37,13 @@ namespace Starfield
             timer.Interval = TimeSpan.FromMilliseconds(1000.0/TARGET_FPS);
             timer.Tick     += Loop;
             timer.Start();
-            fpsTimer.Tick     += FpsCounter;
-            fpsTimer.Start();
             fpsTimer2.Interval =  new TimeSpan(0, 0, 0, 1);
             fpsTimer2.Tick += FpsCounter2;
             fpsTimer2.Start();
 
-
-            oldH = (int)offsetH;
-            oldV = (int)offsetV;
-            targetH = r.Next(-2000, 2000);
-            targetV = r.Next(-2000, 2000);
-
-            offsetterDispatcher.Interval = TimeSpan.FromMilliseconds(100);
-            offsetterDispatcher.Tick += OffsetterDispatcher_Tick;
-            offsetterWatch = new Stopwatch();
-            offsetterWatch.Start();
-            offsetterDispatcher.Start();
-
             clock = new Stopwatch();
-            watch = new Stopwatch();
-            watch.Start();
             
             //ConsoleAllocator.ShowConsoleWindow();
-        }
-
-        private int oldH;
-        private int oldV;
-        private int targetH;
-        private int targetV;
-        private void OffsetterDispatcher_Tick(object sender, EventArgs e)
-        {
-            //Calculate new horizontal offset if target is reached
-            if (oldH < targetH && offsetH > targetH
-             || oldH > targetH && offsetH < targetV)
-            {
-                oldH = (int)offsetH;
-                targetH = r.Next(-2000, 2000);
-            }
-
-
-            var i1 = sliderHorOffset.Value;
-            var x = targetH - oldH + offsetH - (targetH - oldH)/2.0;
-            //var i2 = Math.Abs(targetH - oldH) / (1.0 + Math.Pow(Math.Pow(Math.E, -0.005),x)) - Math.Abs(targetH - oldH)/2.0;
-
-            var i2 = Math.Abs(targetH - oldH) * 2 / (1.0 + Math.Pow(Math.E, -0.01 * x - 500)) - (targetH - oldH)/2.0;
-
-
-            Debug.WriteLine($"{i1 - i2}");
-            sliderHorOffset.Value = i2;
-                
-            
-            
-            //sliderVerOffset.Value += (targetV - oldV) / 100;
-        }
-       
-        private void FpsCounter(object sender, EventArgs e)
-        {
-            frameCount++;
-            if (watch.ElapsedMilliseconds <= 1000) return;
-            watch.Restart();
-            fpsCounter.Content = $"{frameCount.ToString().PadRight(5)} FPS (Render)";
-            frameCount = 0;
         }
 
         private void FpsCounter2(object sender, EventArgs e)
@@ -124,9 +62,9 @@ namespace Starfield
 
             for( var i = 0; i < STAR_NBR; i++ )
             {
-                var star = new Star(X: r.Next(-2000, 2000)/100.0,
-                                    Y: r.Next(-2000, 2000)/100.0,
-                                    Z: r.Next(1,   MAX_DEPTH));
+                var star = new Star(r.Next(-2000, 2000)/100.0,
+                                    r.Next(-2000, 2000)/100.0,
+                                    r.Next(1,   MAX_DEPTH));
 
                 var k  = kValue / star.z;
                 var px = star.x * k + halfWidth;
@@ -191,13 +129,9 @@ namespace Starfield
 
             if (star.z <= 0 || (posVals.px <=0 || posVals.px >= canvas.ActualWidth) || (posVals.py <= 0 || posVals.py >= canvas.ActualHeight))
             {
-                do
-                {
-                    star.x = r.Next(-2000, 2000) / 100.0;
-                    star.y = r.Next(-2000, 2000) / 100.0;
-                    star.z = r.Next(MAX_DEPTH /2, MAX_DEPTH);
-                }
-                while (Math.Abs(star.x) < 0.0 || Math.Abs(star.y) < 0.0);
+                star.x = r.Next(-2000, 2000) / 100.0;
+                star.y = r.Next(-2000, 2000) / 100.0;
+                star.z = r.Next(MAX_DEPTH /2, MAX_DEPTH);
                 oldZ = star.z;
                 posVals = CalcPos(star.x, star.y, kValue / star.z);
             }
@@ -207,23 +141,32 @@ namespace Starfield
             var shade = Convert.ToInt32((1 - star.z / MAX_DEPTH) * 255);
             var color = Color.FromArgb(255, (byte) shade, (byte) shade, (byte) shade);
 
-            (Dispatcher ?? throw new InvalidOperationException()).BeginInvoke(() =>
-            {
-                star.tail.Stroke = new SolidColorBrush(color);
-                star.tail.X1     = posVals.px;
-                star.tail.Y1     = posVals.py;
-                star.tail.X2     = oldVals.px;
-                star.tail.Y2     = oldVals.py;
+            star.tail.Stroke = new SolidColorBrush(color);
 
-                Canvas.SetLeft(star.tail, 0);
-                Canvas.SetTop(star.tail, 0);
-            },DispatcherPriority.Send);
+            star.tail.X1 = posVals.px;
+            star.tail.Y1 = posVals.py;
+            star.tail.X2 = oldVals.px;
+            star.tail.Y2 = oldVals.py;
+
+            Canvas.SetLeft(star.tail, 0);
+            Canvas.SetTop(star.tail, 0);
+            //(Dispatcher ?? throw new InvalidOperationException()).BeginInvoke(() =>
+            //{
+            //    star.tail.Stroke = new SolidColorBrush(color);
+            //    star.tail.X1     = posVals.px;
+            //    star.tail.Y1     = posVals.py;
+            //    star.tail.X2     = oldVals.px;
+            //    star.tail.Y2     = oldVals.py;
+
+            //    Canvas.SetLeft(star.tail, 0);
+            //    Canvas.SetTop(star.tail, 0);
+            //},DispatcherPriority.Send);
         }
 
         private (double px, double py) CalcPos(double origX, double origY, double k)
         {
-            var px = origX * k + halfWidth + offsetH / 10;
-            var py = origY * k + halfHeight + offsetV / 10;
+            var px = origX * k + halfWidth;
+            var py = origY * k + halfHeight;
 
             return (px, py);
         }
@@ -235,24 +178,6 @@ namespace Starfield
         /// <param name="e"></param>
         private void Slider_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
         { kValue = e.NewValue; }
-
-        private void sliderOffset_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
-        {
-            if (sender is Slider offset)
-            {
-                switch (offset.Name)
-                {
-                    case "sliderHorOffset":
-                        offsetH = e.NewValue;
-                        break;
-                    case "sliderVerOffset":
-                        offsetV = e.NewValue;
-                        break;
-                    default:
-                        break;
-                }
-            }
-        }
     }
 
     /// <summary>
